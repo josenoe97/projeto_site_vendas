be276588691324b90ceae2e852649dc5c92331a6
Junior <edsonjunior9701@outlook.com>
2024-09-19 22:59:07 -0300

    adicionando frontend no git

---
+++ src/app/products/features/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, inject } from '@angular/core';
+import { ProductsStateService } from '../../data-access/product-state.service';
+import { ProductCardComponent } from '../../ui/product-card/product-card.component';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [ProductCardComponent],
+  templateUrl: './product-list.component.html',
+  providers: [ProductsStateService],
+})
+
+export default class ProductListComponent {
+  productsState = inject(ProductsStateService);
+}
+
